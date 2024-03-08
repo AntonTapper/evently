08401a47324e6c719211fda42e2e401faa1b3e1c
AntonTapper <ant.tapper@gmail.com>
2024-03-08 19:46:16 +0100

    wip

---
+++ components/shared/Collection.tsx
@@ -0,0 +1,40 @@
+import { IEvent } from '@/lib/database/models/event.model'
+import React from 'react'
+
+type CollectionProps = {
+    data: IEvent[],
+    emptyTitle: string,
+    emptyStateSubtext: string,
+    limit: number,
+    page: number | string,
+    totalPages?: number,
+    urlParamName?: string,
+    collectionType?: 'Events_Organzied' | 'My_Tickets' | 'All_Events'
+}
+
+const Collection = ({
+    data,
+    emptyTitle,
+    emptyStateSubtext,
+    page,
+    totalPages,
+    collectionType,
+    urlParamName
+}:CollectionProps) => {
+  return (
+    <>
+        {data.length > 0 ? (
+            <div>
+                {data[0].title}
+            </div>
+        ): (
+            <div className='flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center'>
+                <h3 className='p-bold-20 md:h5-bold'>{emptyTitle}</h3>
+                <p className='p-regular-14'>{emptyStateSubtext}</p>
+            </div>
+        )}
+    </>
+  )
+}
+
+export default Collection
