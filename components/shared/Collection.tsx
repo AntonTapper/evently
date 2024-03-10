e4095343c52cc617c92dfa6f1dc21cc3e27bb016
AntonTapper <ant.tapper@gmail.com>
2024-03-10 21:18:04 +0100

    Card wip

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,5 +1,6 @@
 import { IEvent } from '@/lib/database/models/event.model'
 import React from 'react'
+import Card from './Card'
 
 type CollectionProps = {
     data: IEvent[],
@@ -24,8 +25,19 @@ const Collection = ({
   return (
     <>
         {data.length > 0 ? (
-            <div>
-                {data[0].title}
+            <div className='flex flex-col items-center gap-10'>
+                <ul className='grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10'>
+                    {data.map((event) => {
+                        const hasOrderLink = collectionType === 'Events_Organzied'
+                        const hidePrice = collectionType === 'My_Tickets'
+
+                        return (
+                            <li key={event._id} className='flex justify-center'>
+                                <Card event={event} hasOrderLink={hasOrderLink} hidePrice={hidePrice}/>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         ): (
             <div className='flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center'>
